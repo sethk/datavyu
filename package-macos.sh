68fa687dd4c2d4818dbae0769da85f054e65bde4
Reda Nezzar <nezzar.reda@gmail.com>
2018-04-11 18:13:08 -0400

    DVD-239: package and disk image creation script for macos

--- package-macos.sh
+++ package-macos.sh
@@ -1,10 +1,9 @@
-#! /bin/#!/usr/bin/env bash
+#!/usr/bin/env bash
 mvn clean -Dmaven.test.skip=true jfx:native
 
-cp -R target/jfx/native/ target/jfx/native/Datavyu
+ln -s /Applications target/jfx/native/
 
-ln -s /Applications target/jfx/native/Datavyu/
-
+echo version number:
 read version
 
-sudo hdiutil create ~/Desktop/Datavyu-X.X.X-PLATFORM.dmg -volname "Datavyu" -srcfolder target/jfx/native/Datavyu/
+sudo hdiutil create target/jfx/native/Datavyu-$version-OSX.dmg -volname "Datavyu" -srcfolder target/jfx/native/
