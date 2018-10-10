a14a5e86d072fed1b6f6d555ac234443bb698e01
Reda Nezzar <nezzar.reda@gmail.com>
2018-10-10 18:03:30 -0400

    Add MPV player plugin with new ffmpeg dependency version

---
+++ src/main/java/org/datavyu/plugins/mpv/MpvPlayer.java
@@ -0,0 +1,169 @@
+package org.datavyu.plugins.mpv;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.datavyu.plugins.ffmpeg.MediaPlayer;
+import org.datavyu.plugins.ffmpeg.MpvMediaPlayer;
+import org.datavyu.plugins.ffmpeg.PlayerStateEvent;
+import org.datavyu.plugins.ffmpegplayer.AudioSoundStreamListener;
+import org.datavyu.util.NativeLibraryLoader;
+
+import javax.sound.sampled.AudioFormat;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.io.File;
+
+public class MpvPlayer extends JPanel {
+
+    /** Identifier for object serialization */
+    private static final long serialVersionUID = 5109839668203738974L;
+
+    /** The logger for this class */
+    private static Logger logger = LogManager.getFormatterLogger(MpvPlayer.class);
+
+    static {
+        try {
+            logger.info("Extracting libraries for MPV .");
+            NativeLibraryLoader.extract("MpvMediaPlayer");
+        } catch (Exception e) {
+            logger.error("Failed loading MPV libraries due to error: ", e);
+        }
+    }
+	
+	/** The movie stream for this movie player */
+	private MediaPlayer mediaPlayer;
+
+	/**
+	 * Construct an MpvPlayer by creating the underlying movie stream provider
+	 * and registering stream listeners for the video and audio. The stream
+	 * listener for the video will show the image in this JPanel.
+	 *
+	 * @param viewer The ffmpeg viewer
+     * @param sourceFile The source file
+	 */
+	MpvPlayer(MpvStreamViewer viewer, File sourceFile) {
+		setLayout(new BorderLayout());
+		try {
+			mediaPlayer = new MpvMediaPlayer(sourceFile.toURI(), viewer);
+			mediaPlayer.init();
+		}catch (Exception e) {
+			logger.error("Cannot initialize MPV player due to error: ", e);
+		}
+	}
+
+	/**
+	 * Get the duration of the opened video/audio stream of this player in seconds.
+	 *
+	 * @return Duration of the opened stream.
+	 */
+	public double getDuration() {
+		return mediaPlayer.getDuration();
+	}
+
+	/**
+	 * Get the original stream size (not the size when a viewing window is used).
+	 *
+	 * @return Original stream size: width, height.
+	 */
+	public Dimension getOriginalVideoSize() {
+		return new Dimension(mediaPlayer.getImageWidth(), mediaPlayer.getImageHeight());
+	}
+
+	/**
+	 * Get the current time in seconds.
+	 *
+	 * @return Current time in seconds.
+	 */
+	public double getCurrentTime() {
+		return mediaPlayer.getPresentationTime();
+	}
+
+	/**
+	 * Seek to the position.
+	 *
+	 * @param position Position in seconds.
+	 */
+	public void setCurrentTime(double position) {
+        logger.info("Seeking position: " + position);
+		mediaPlayer.seek(position);
+    }
+
+	/**
+	 * Clean up the player before closing.
+	 */
+	public void cleanUp() {
+	    logger.info("Closing stream.");
+		mediaPlayer.dispose();
+	}
+
+	/**
+	 * Set the start back speed for this player.
+	 *
+	 * @param playbackSpeed The start back speed.
+	 */
+	public void setPlaybackSpeed(float playbackSpeed) {
+		logger.info("Setting start back speed: " + playbackSpeed);
+		// Not implemented yet, we are using the fake playback
+		mediaPlayer.setRate(playbackSpeed);
+	}
+
+	/**
+	 * Play the video/audio.
+	 */
+	public void play() {
+		logger.info("Starting the video");
+		mediaPlayer.play();
+	}
+
+	/**
+	 * Stop the video/audio.
+	 */
+	public void stop() {
+		logger.info("Stopping the video.");
+		mediaPlayer.stop();
+	}
+
+	@Deprecated
+	public void setScale(float scale) {
+//	    displayStreamListener.setScale(scale);
+    }
+
+	/**
+	 * Instead of isPlaying a sequence of frames just step by one frame.
+	 */
+	public void stepForward() {
+	    logger.info("Step forward.");
+		mediaPlayer.stepForward();
+	}
+
+	public void stepBackward() {
+		logger.info("Step backward.");
+		mediaPlayer.stepBackward();
+	}
+
+	/**
+	 * Set the audio volume.
+	 *
+	 * @param volume New volume to set.
+	 */
+	public void setVolume(float volume) {
+		mediaPlayer.setVolume(volume);
+	}
+
+	public void setMute(final boolean newMute) {
+		mediaPlayer.setMute(newMute);
+	}
+
+	public boolean isMute() {
+		return mediaPlayer.getMute();
+	}
+
+	boolean isPlaying() {
+	    return mediaPlayer.getState() == PlayerStateEvent.PlayerState.PLAYING;
+    }
+
+    public double getFPS() {
+    return mediaPlayer.getFps();
+	}
+}
---
+++ src/main/java/org/datavyu/plugins/mpv/MpvStreamViewer.java
@@ -0,0 +1,162 @@
+package org.datavyu.plugins.mpv;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.datavyu.models.Identifier;
+import org.datavyu.plugins.StreamViewerDialog;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class MpvStreamViewer extends StreamViewerDialog {
+
+    /** The logger for this class */
+    private static Logger logger = LogManager.getFormatterLogger(MpvStreamViewer.class);
+
+    /** The player this viewer is displaying */
+    private MpvPlayer player;
+
+    /** Currently is seeking */
+    private boolean isSeeking = false;
+
+    MpvStreamViewer(final Identifier identifier, final File sourceFile, final Frame parent, final boolean modal) {
+        super(identifier, parent, modal);
+        logger.info("Opening file: " + sourceFile.getAbsolutePath());
+        player = new MpvPlayer(this, sourceFile);
+        setSourceFile(sourceFile);
+    }
+
+    private void launch(Runnable task) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            task.run();
+        } else {
+            try {
+                SwingUtilities.invokeLater(task);
+            } catch (Exception e) {
+                logger.error("Failed task. Error: ", e);
+            }
+        }
+    }
+
+    @Override
+    protected void setPlayerVolume(float volume) {
+        player.setVolume(volume);
+    }
+
+    @Override
+    protected Dimension getOriginalVideoSize() {
+        return player.getOriginalVideoSize();
+    }
+
+    @Override
+    public void setCurrentTime(long time) {
+        launch(() -> {
+            try {
+                logger.info("Set time to: " + time + " milliseconds.");
+                if (!isSeeking) {
+                    EventQueue.invokeLater(() -> {
+                        isSeeking = true;
+                        player.setCurrentTime(time / 1000.0);
+                        isSeeking = false;
+                    });
+                }
+            } catch (Exception e) {
+                logger.error("Unable to set time to " + time + " milliseconds, due to error: ", e);
+            }
+        });
+    }
+
+    @Override
+    public void start() {
+        launch(() -> {
+            if (!isPlaying()) {
+                player.play();
+            }
+        });
+    }
+
+    @Override
+    public void stop() {
+        launch(() -> {
+            if (isPlaying()) {
+                player.stop();
+            }
+        });
+    }
+
+    @Override
+    public void setRate(float speed) {
+        launch(() -> {
+            playBackRate = speed;
+            if(isSeekPlaybackEnabled()){
+                player.setMute(true);
+            }else{
+                player.setMute(false);
+            }
+            if (speed == 0) {
+                player.stop();
+            } else {
+                player.setPlaybackSpeed(speed);
+            }
+        });
+    }
+
+    @Override
+    protected float getPlayerFramesPerSecond() { return (float) player.getFPS(); }
+
+    @Override
+    public long getDuration() {
+        return (long) (player.getDuration() * 1000);
+    }
+
+    @Override
+    public long getCurrentTime() {
+        return (long) (player.getCurrentTime() * 1000);
+    }
+
+    @Override
+    protected void cleanUp() {
+        player.cleanUp();
+    }
+
+    @Override
+    public void stepForward() {
+        launch(() -> {
+            player.stepForward();
+        });
+    }
+
+    @Override
+    public void stepBackward() {
+        launch(() -> {
+            player.stepBackward();
+        });
+    }
+
+    @Override
+    protected void resizeVideo(float scale) {
+        super.resizeVideo(scale);
+        logger.info("Resizing video to scale %2.2f", scale);
+        player.setScale(scale);
+        notifyChange();
+    }
+
+    @Override
+    public boolean isStepEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return player != null && player.isPlaying();
+    }
+
+//    @Override
+//    public boolean isSeekPlaybackEnabled() { return playBackRate > 1F
+//                                                || playBackRate < 0F
+//                                                || (playBackRate > 0F && playBackRate < 1F); }
+
+    @Override
+    public boolean isSeekPlaybackEnabled() { return playBackRate < 0F || playBackRate > 32F; }
+}
