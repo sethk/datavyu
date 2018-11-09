50832a9edbcb45f93dc9979ede4837a95e70bb75
Reda Nezzar <nezzar.reda@gmail.com>
2018-11-09 15:16:44 -0500

    Enable stepping for MPV Player

--- src/main/java/org/datavyu/plugins/mpv/MpvStreamViewer.java
+++ src/main/java/org/datavyu/plugins/mpv/MpvStreamViewer.java
@@ -144,7 +144,7 @@ public class MpvStreamViewer extends StreamViewerDialog {
 
     @Override
     public boolean isStepEnabled() {
-        return false;
+        return true;
     }
 
     @Override
